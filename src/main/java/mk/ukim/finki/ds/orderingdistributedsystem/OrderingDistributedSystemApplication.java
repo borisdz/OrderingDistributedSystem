a32abfc2ffa989583cf4cf092d91e6eb852b826a
Boris Djotov <borisdzotov@gmail.com>
2025-12-09 18:35:02 +0100

    Project skeleton

---
+++ src/main/java/mk/ukim/finki/ds/orderingdistributedsystem/OrderingDistributedSystemApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ds.orderingdistributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingDistributedSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingDistributedSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/ds/orderingdistributedsystem/OrderingDistributedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ds.orderingdistributedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingDistributedSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
