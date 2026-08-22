3953eb92500a9f53d72a3624f20bfb347d8a2194
Boris Djotov <borisdzotov@gmail.com>
2026-08-22 15:17:39 +0200

    Implement idempotency in order creation: add IdempotencyRecord and IdempotencyRepository, update OrderService and OrderController to handle idempotency keys

--- src/main/java/mk/ukim/finki/ds/orderingdistributedsystem/web/OrderController.java
+++ src/main/java/mk/ukim/finki/ds/orderingdistributedsystem/web/OrderController.java
@@ -5,6 +5,7 @@ import mk.ukim.finki.ds.orderingdistributedsystem.CreateOrderRequest;
 import mk.ukim.finki.ds.orderingdistributedsystem.service.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,10 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<String> createOrder(@RequestBody CreateOrderRequest request){
-        String orderId = orderService.createOrder(request);
+    public ResponseEntity<String> createOrder(
+            @RequestHeader(value = "X-Idempotency-Key", required = false) String idempotencyKey,
+            @RequestBody CreateOrderRequest request){
+        String orderId = orderService.createOrder(request, idempotencyKey);
         return ResponseEntity.accepted().body("Order accepted: " + orderId);
     }
 }
